4992575b03176c0a8fbe23f1bfa96068c126aae4
Real-201398087 <122286589+Real-201398087@users.noreply.github.com>
2023-06-13 15:18:01 -0400

    requis 4 fail

---
+++ partie1/processor.test.js
@@ -0,0 +1,26 @@
+const processor = require('./processor.js');
+
+describe("transmission processor", function () {
+
+    
+    test("takes a string and returns an object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(typeof result).toEqual("object");
+    });
+
+    test("returns -1 if '::' not found", function () {
+        let result = processor("9701<489584872710>");
+        expect(result).toEqual(-1);
+    });
+
+    test("returns id in object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).not.toEqual(undefined);
+    });
+    
+    test("returns id is a number", function () {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).toBeNumber()
+    });
+
+});
